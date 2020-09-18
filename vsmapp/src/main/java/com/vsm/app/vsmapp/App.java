91415514a2364b78789875b8ab0cff00fd007aee
vsm <devops.july2.2017@gmail.com>
2020-09-18 20:04:59 +0530

    updated files for vsmapp

---
+++ vsmapp/src/main/java/com/vsm/app/vsmapp/App.java
@@ -0,0 +1,13 @@
+package com.vsm.app.vsmapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
